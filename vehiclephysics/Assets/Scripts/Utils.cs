a560cfebbf0b3ed8eac1e26d366959db25012f40
LAPTOP-UUFD5PVK\TaraO <Tara.OP@hotmail.com>
2020-10-18 22:35:50 +0100

    changed projects-particles smaller
    I have been playing around with the particle physics system and have made them to an appropriate size, havent figures out the coding bit yet.

---
+++ vehiclephysics/Assets/Scripts/Utils.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Utils
+{
+    public static void DrawArrow(in Vector3 pos, in Vector3 direction, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        Debug.DrawRay(pos, direction);
+        DrawTip(pos, direction, Gizmos.color, arrowHeadLength, arrowHeadAngle);
+    }
+
+    public static void DrawArrow(in Vector3 pos, in Vector3 direction, in Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        Debug.DrawRay(pos, direction, color);
+        DrawTip(pos, direction, color, arrowHeadLength, arrowHeadAngle);
+    }
+
+    private static void DrawTip(in Vector3 pos, in Vector3 direction, in Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        var right = Quaternion.LookRotation(direction) * Quaternion.Euler(arrowHeadAngle, 0, 0) * Vector3.back * arrowHeadLength;
+        var left = Quaternion.LookRotation(direction) * Quaternion.Euler(-arrowHeadAngle, 0, 0) * Vector3.back * arrowHeadLength;
+        var up = Quaternion.LookRotation(direction) * Quaternion.Euler(0, arrowHeadAngle, 0) * Vector3.back * arrowHeadLength;
+        var down = Quaternion.LookRotation(direction) * Quaternion.Euler(0, -arrowHeadAngle, 0) * Vector3.back * arrowHeadLength;
+        var end = pos + direction;
+        Debug.DrawRay(end, right, color);
+        Debug.DrawRay(end, left, color);
+        Debug.DrawRay(end, up, color);
+        Debug.DrawRay(end, down, color);
+    }
+}
---
+++ vehiclephysics/Assets/Scripts/VehicleController1.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using TMPro;
+using UnityEngine;
+
+public class VehicleController1 : MonoBehaviour
+{
+    private float verticalInput;
+    private float horizontalInput;
+    private Rigidbody chassis;
+    public float forceImpulse;
+    public float torqueImpulse;
+
+    VehicleController1()
+    {
+        forceImpulse = 0.0f;
+        torqueImpulse = 0.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        verticalInput = 0.0f;
+        horizontalInput = 0.0f;
+        chassis = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetKeys();   
+    }
+    void FixedUpdate()
+    {
+        UpdateController();
+    }
+
+    void UpdateController()
+    {
+        if(Mathf.Abs(verticalInput) > 0.0f)
+        {
+            // Calculate the impulse
+            Vector3 vimpulse = transform.forward * forceImpulse * verticalInput;
+            // Draw the impulse 
+            Utils.DrawArrow(transform.position, vimpulse, Color.blue);
+            // Apply the impulse
+            chassis.AddForce(vimpulse, ForceMode.Impulse);
+        }
+
+        if(Mathf.Abs(horizontalInput) > 0.0f)
+        {
+            Vector3 himpulse = transform.up * forceImpulse * verticalInput;
+
+            Utils.DrawArrow(transform.position, himpulse, Color.green);
+
+            chassis.AddTorque(himpulse, ForceMode.Impulse);
+        }
+    }
+
+    // Get use input
+    void GetKeys()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    //Collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Box")
+        {
+            Debug.Log("CollisionSuccess");
+           // ParticleSystem part = GetComponent<ParticleSystem>;
+            Destroy(GameObject.FindWithTag("Box"));
+            
+        }
+    }
+}
