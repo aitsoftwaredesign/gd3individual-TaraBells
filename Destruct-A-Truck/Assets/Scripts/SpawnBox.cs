35ac7e27577f1296018797ef844a3784580fc45e
LAPTOP-UUFD5PVK\TaraO <Tara.OP@hotmail.com>
2020-11-02 12:00:52 +0000

    attempt at spawning boxes
    created a spawn method but trying to instantiate it from an array

---
+++ Destruct-A-Truck/Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBox : MonoBehaviour
+{
+    private float nextSpawnTime;
+    public GameObject[] spawnPoints;
+    private int spIndex = 0;
+
+    public GameObject[] pattern;
+    private int patternIndex = 0;
+
+
+    [SerializeField]
+    private GameObject boxPrefab;
+    [SerializeField]
+    private float spawnDelay = 5;
+
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (ShouldSpawn())
+        {
+            SpawnBoxes();
+                 }
+        
+    }
+
+    private void SpawnBoxes()
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            nextSpawnTime = Time.time + spawnDelay;
+        }
+
+        Instantiate(boxPrefab, transform.position, transform.rotation);
+        }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+
+   
+}
