7efc1111f708d45c1c9f9e90675e3fe16b065698
LAPTOP-UUFD5PVK\TaraO <Tara.OP@hotmail.com>
2020-11-08 21:28:39 +0000

    countdown timer added.
    i have a countdown time to let the player know how much time they have in the room to collect points. It does not yet restart the game

---
+++ Destruct-A-Truck/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startTime = 10f;
+    public Text countdownText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
--- Destruct-A-Truck/Assets/Scripts/KeepScore.cs
+++ Destruct-A-Truck/Assets/Scripts/KeepScore.cs
@@ -6,20 +6,11 @@ public class KeepScore : MonoBehaviour
 {
     public static int Score = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+  
 
     void OnGUI()
     {
-        GUI.Box(new Rect(0, 0, 100, 50), "Score: "  + Score);
+        GUI.Box(new Rect(50, 50, 200, 50), "Score is: "  + Score);
     }
 
 }
