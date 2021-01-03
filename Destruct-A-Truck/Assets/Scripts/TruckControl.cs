f12498a06efebf293cfc17d22b6cff24af1761e5
LAPTOP-UUFD5PVK\TaraO <Tara.OP@hotmail.com>
2021-01-03 01:24:30 +0000

    level 2 messing with camera, score trying to display, truck control fixed

--- Destruct-A-Truck/Assets/Scripts/TruckControl.cs
+++ Destruct-A-Truck/Assets/Scripts/TruckControl.cs
@@ -6,21 +6,38 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class TruckControl : MonoBehaviour
 {
+    public static int Score = 0;
+    // Start is called before the first frame update
+
     private float verticalInput;
     private float horizontalInput;
-    private Rigidbody truck;
+    public Rigidbody truck;
     public float forceImpulse;
     public float torqueImpulse;
     public bool onGround;
     public float linearGrip;
     public float angularGrip;
 
+    public float wheelBase;
+    public float axelWidth;
+    public float suspensionHeight;
+    public float wheelRadius;
+    public float suspensionStrength;
+    public float suspensionDamping;
+
     TruckControl()
     {
+        //initialize
         forceImpulse = 0.0f;
         torqueImpulse = 0.0f;
         linearGrip = 0.2f;
         angularGrip = 0.2f;
+        wheelBase = 1.0f;
+        axelWidth = 8.86f;
+        suspensionHeight = 1f;
+        wheelRadius = 2f;
+        suspensionStrength = 40.0f;
+        suspensionDamping = 2.0f;
     }
 
     // Start is called before the first frame update
@@ -28,18 +45,24 @@ public class TruckControl : MonoBehaviour
     {
         verticalInput = 0.0f;
         horizontalInput = 0.0f;
-        onGround = false;
-        truck = gameObject.GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         GetKeys();
+
+        void OnGUI()
+        {
+            GUI.Box(new Rect(50, 50, 200, 50), "Score is: " + Score);
+        }
+
     }
     void FixedUpdate()
     {
         UpdateController();
+        Suspend();
     }
 
     void UpdateController()
@@ -92,13 +115,69 @@ public class TruckControl : MonoBehaviour
         Vector3 gripImp = currentRight * (-slideSpeed * mass * linearGrip);
         truck.AddForce(gripImp, ForceMode.Impulse);
 
-        // Vector3.Dot(angularVelocity, currentUp);
+        Vector3.Dot(angularVelocity, currentUp);
         // How much is our angularVelocity pointing in our up direction? i.e. Are we spinning?
-        ///float spinSpeed = Vector3.Dot(angularVelocity, currentUp);
-        //Debug.Log("Slide speed: " + slideSpeed);
+        float spinSpeed = Vector3.Dot(angularVelocity, currentUp);
+        Debug.Log("Slide speed: " + slideSpeed);
         // Apply an angular impulse to compensate for spinning
-        ///Vector3 antiSpinImp = currentUp * (-spinSpeed * mass * angularGrip);
-        ///chassis.AddTorque(antiSpinImp, ForceMode.Impulse);
+        Vector3 antiSpinImp = currentUp * (-spinSpeed * mass * angularGrip);
+        truck.AddTorque(antiSpinImp, ForceMode.Impulse);
+    }
+
+    void Suspend()
+    {
+        Vector3 wheelPos = default;
+        float totalSuspensionDist = suspensionHeight + wheelRadius;
+        // Get the chassis down direction in world space
+        Vector3 wd = transform.up;
+           
+            // Get the wheel position in world space
+            Vector3 wheelPosWs = transform.TransformPoint(wheelPos);
+
+            // Draw the ray we are about to cast
+            Debug.DrawRay(wheelPosWs,
+                -transform.up * totalSuspensionDist,
+                Color.magenta);
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(wheelPosWs,
+                -transform.up,
+                out hit, totalSuspensionDist))
+            {
+                if (hit.distance < totalSuspensionDist)
+                {
+                    // Total Force = SuspensionForce - DampingForce 
+                    Vector3 diff = hit.point - wheelPosWs;
+
+                    // Calculate the suspension force
+                    float suspensionForce = suspensionStrength *
+                        (totalSuspensionDist - diff.magnitude);
+
+                    // Calculate the damping force. The higher the velocity of the wheelPos point
+                    // on the body the higher the damping force
+                    Vector3 pointVelocity = truck.GetPointVelocity(wheelPosWs);
+                    // How much is the point velocity pointing in the direction of the diff vector 
+                    float dampingScale = Vector3.Dot(pointVelocity, diff);
+                    float dampingForce = suspensionDamping * dampingScale;
+
+                    // Apply the force in the direction of the diff vector and scale by mass
+                    Vector3 force = (suspensionForce + dampingForce) *
+                    -diff.normalized * truck.mass;
+                    //Vector3 force = suspensionForce * -diff.normalized * chassis.mass;
+                    truck.AddForceAtPosition(force, wheelPosWs);
+                }
+                // Update the wheel position based on the hit distance
+                _ = wheelPosWs +
+                        (-transform.up * (hit.distance - wheelRadius));
+            }
+            else
+            {
+                // Update the wheel position
+
+            }
+
+        
     }
 
     void OnCollisionEnter(Collision collision)
@@ -106,6 +185,7 @@ public class TruckControl : MonoBehaviour
         if (collision.gameObject.tag == "plane")
         {
             onGround = true;
+           
         }
     }
 
@@ -114,6 +194,8 @@ public class TruckControl : MonoBehaviour
         if (collision.gameObject.tag == "plane")
         {
             onGround = false;
+            truck.angularVelocity = Vector3.zero;
+            truck.velocity = Vector3.zero;
         }
     }
 
@@ -123,4 +205,6 @@ public class TruckControl : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
     }
+
+
 }
